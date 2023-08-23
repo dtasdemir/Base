a06f6b3d7cd82991994ca16afbe8c969c0932968
derya <derya@aprilerp.com>
2023-08-23 22:24:47 +0300

    Splash Screen Completed

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -4,25 +4,29 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useDispatch } from 'react-redux';
 import { LoginUser } from '../redux/authSlice';
 
+import m from '../style/index';
 export default function Splash({navigation}) {
 
   const dispatch = useDispatch();
   
   useEffect(() => {
-    AsyncStorage.getItem('Base::user').then(p => {
-      if(p) {
-        dispatch(LoginUser(JSON.parse(p)))
-      } else {
-        navigation.navigate('login');
-      }
-    }).catch(e => {
-      console.log(e)
-    })
+    
+    setTimeout(() => {
+      AsyncStorage.getItem('Base::user').then(p => {
+        if(p) {
+          dispatch(LoginUser(JSON.parse(p)))
+        } else {
+          navigation.navigate('login');
+        }
+      }).catch(e => {
+        console.log(e)
+      })
+    }, 1000)
 
   },[])
   return (
-    <View>
-      <Text>Splash Screen</Text>
+    <View style={[m.f(1), m.bg(m.primaryColor), m.vcenter, m.hcenter]}>
+      <Text style={[m.fontSCW(24, m.white, '600')]}>Splash Screen</Text>
     </View>
   );
 }
